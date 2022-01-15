ac3d3cee649359f9b9f91a36b4bcda738f76e88c
Yogue1997 <yousyogue@gmail.com>
2022-01-15 11:49:50 -0500

    complete

---
+++ src/components/Input.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export default class Input extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            item: "",
+            units: "",
+            quantity: "",
+            isPurchased: false,
+        };
+    }
+
+    handleChange = (event) => {
+
+        if (event.target.id === 'item') {
+            this.setState({ item: event.target.value })
+
+        } else if (event.target.id === 'units') {
+            this.setState({ units: event.target.value })
+
+        } else {
+            this.setState({ quantity: event.target.value })
+        }
+
+
+        // Computed property name
+
+        // this.setState({ [event.target.id]: event.target.value });
+    };
+
+    handleSubmit = (event) => {
+        ///    onClick ... onChange ... onSubmit
+        event.preventDefault();
+        this.props.oneGrocery(this.state)
+        // trying to use a function 
+        console.log(this.state);
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="cf">
+
+                <label htmlFor="item">item:</label>
+                <input
+                    id="item"
+                    onChange={this.handleChange}
+                    value={this.state.item}
+                />
+
+                <label htmlFor="units">units:</label>
+                <input
+                    id="units"
+                    onChange={this.handleChange}
+                    value={this.state.units}
+
+                />
+
+                <label htmlFor="quantity">quantity:</label>
+                <input
+                    id="quantity"
+                    onChange={this.handleChange}
+                    value={this.state.quantity}
+                />
+
+                <button>Submit</button>
+            </form>
+        );
+    }
+}
